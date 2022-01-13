1842e08d4024395ca1f4b990f5590b542e3a652e
iuliki <iulia.bulugu@emanuel.ro>
2022-01-13 13:02:44 +0200

    start

---
+++ recursivitate1/recursivitate1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace recursivitate1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] array = { 1, 34, 3, 78, 5, 6, 7, 8 };
+            int n;
+            AfisareVector(array, n);
+           
+        }
+        static int AfisareVector(int[] array,int  n)
+        {
+            if (n == 1)
+                Console.WriteLine(array[n]);
+            else
+                return AfisareVector(array, n - 1);
+        }
+    }
+}
