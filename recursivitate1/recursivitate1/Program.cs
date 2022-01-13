2ec00051857ed4faee3c7528c20743c3a413f6f7
iuliki <iulia.bulugu@emanuel.ro>
2022-01-13 15:35:59 +0200

    rezolvat

--- recursivitate1/recursivitate1/Program.cs
+++ recursivitate1/recursivitate1/Program.cs
@@ -9,16 +9,10 @@ namespace recursivitate1
             Console.WriteLine("Hello World!");
 
             int[] array = { 1, 34, 3, 78, 5, 6, 7, 8 };
-            int n;
-            AfisareVector(array, n);
+            
+            recursiv.AfisareVector(array, array.Length);
            
         }
-        static int AfisareVector(int[] array,int  n)
-        {
-            if (n == 1)
-                Console.WriteLine(array[n]);
-            else
-                return AfisareVector(array, n - 1);
-        }
+       
     }
 }
---
+++ recursivitate1/recursivitate1/recursiv.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace recursivitate1
+{
+    public static class recursiv
+    {
+        public static void AfisareVector(int[] array, int n)
+        {
+            if (n == 1)
+                Console.WriteLine(array[n-1]); 
+            else
+            {
+                
+                AfisareVector(array, n - 1);
+              Console.WriteLine(array[n-1]);
+
+            }
+        }
+    }
+}
